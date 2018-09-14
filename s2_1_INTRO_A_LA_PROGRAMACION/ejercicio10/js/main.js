3d96aaa7f4ed71600fb5308e43cad9551f787d46
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-09-14 09:15:23 +0100

    add exercise10

---
+++ s2_1_INTRO_A_LA_PROGRAMACION/ejercicio10/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var user = prompt('Please, Introduce an user role.');
+var funcionality = prompt('Please, Introduce a funcionality.');
+var reason = prompt('Please, Introduce the reason.');
+
+alert('I as a ' + user + ' user, I need ' + funcionality + ' with the purpose of ' + reason + '.');
