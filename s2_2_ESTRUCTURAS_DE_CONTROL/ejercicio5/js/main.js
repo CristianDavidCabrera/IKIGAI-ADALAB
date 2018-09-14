652aa41292e074d4bcbbe05888ff5a6a1ccce3f2
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-09-14 11:12:05 +0100

    add exercise5 and exercise6

---
+++ s2_2_ESTRUCTURAS_DE_CONTROL/ejercicio5/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var result = 0;
+
+for(var i = 0; i<10; i++){
+  result = result + 2;
+}
+console.log('El resultado total es: ' + result);
---
+++ s2_2_ESTRUCTURAS_DE_CONTROL/ejercicio6/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var moon = 2017;
+
+console.log('La luna del cazador se verá en los siguientes años: ');
+
+for(var i = 0; i<15; i++){
+  moon = moon + 3;
+  console.log(moon);
+}
