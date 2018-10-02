25743f5db8b5ba10fac383c1a5cbe35168fa634e
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-10-02 09:21:41 +0100

    Add exercise 3 and exercises 1, 2 and 3 are documented

--- s2_6_EVENTOS/ejercicio2/js/main.js
+++ s2_6_EVENTOS/ejercicio2/js/main.js
@@ -1,7 +1,19 @@
-var button = document.getElementById("btn");
-button.addEventListener('click', showMessage);
+/**
+ * 
+ * 1 - Referencia del elemento del DOM.
+ * 2 - Se añade una escucha (con un evento (click) 
+ *     y una funcion que se ejecutará cuando ocurra el evento).
+ * 
+ * 3 - Función para mostrar un mensaje. 
+ *     Toma el valor del elemento refeneciado del DOM (en este caso un input) 
+ *     y muestra un mensaje con el texto Hola más el valor del input.
+ * 
+ */
 
-function showMessage(){
+var button = document.getElementById("btn"); //1
+button.addEventListener('click', showMessage); //2
+
+function showMessage(){ //3
     var msn = document.getElementById("message").value;
     alert("Hola " + msn);
 }
