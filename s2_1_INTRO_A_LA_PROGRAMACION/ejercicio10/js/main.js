cb5a0e458c8de47c1ca20afb4ce0205f72af2212
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-10-31 09:34:06 +0000

    review js concepts: exercise 10

--- s2_1_INTRO_A_LA_PROGRAMACION/ejercicio10/js/main.js
+++ s2_1_INTRO_A_LA_PROGRAMACION/ejercicio10/js/main.js
@@ -4,4 +4,4 @@ var user = prompt('Please, Introduce an user role.');
 var funcionality = prompt('Please, Introduce a funcionality.');
 var reason = prompt('Please, Introduce the reason.');
 
-alert('I as a ' + user + ' user, I need ' + funcionality + ' with the purpose of ' + reason + '.');
+alert('I as a ' + user + ', I need ' + funcionality + ' with the purpose of ' + reason + '.');
